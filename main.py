e1830efcb842620a940f1ad1645cfa7c3062ea17
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-05-23 16:21:32 -0300

    Correcting Meta to goal

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print(2*3**2)
 #Variables
 Sales = 250
 cost = 50
-Meta = 150
+Goal = 150
 profit = Sales - cost
 
 #Print with a variable and a string
@@ -25,19 +25,21 @@ My_age = 5*3
 print("My age is:", My_age)
 
 #If, elif and else
-if profit == Meta:
+if profit == Goal:
     print("Great Job")
-elif profit < Meta:
+elif profit < Goal:
     print("Not bad, but you can do better!")
 else:
     print("excellent Work")
 
-
+#Lists
 List = ["Cpu", "Ram", "Ssd", "Motherboard"]
 print(List[2])
 print(List[1])
 for item in List:
     print(item)
+
+
 Listprice = [200, 150, 100, 300]
 for price in Listprice:
     print(price*2)
