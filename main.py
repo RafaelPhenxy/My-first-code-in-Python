5ae94d51626567278479ddef97ef4535e6eb80fe
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-05-23 09:00:23 -0300

    More Basic

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ print(20-2)
 print(6*3)
 print(36/2)
 print(2*3**2)
+Sales = 200
+cost = 50
+profit = Sales - cost
+print("Your profit is:", profit)
+print("Your cost is:", cost)
+print("Your sales are:", Sales)
+My_age = 5*3
+print("My age is:", My_age)
