0bd3fc6f7bda3a44b73f927e74a369c4351946b8
RafaelPhenxy <rafaelsouzacarlos0@gmail.com>
2026-05-23 15:01:03 -0300

    More Basic

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+#Print with a string
 print("Olá, gringa! Já Já to ai...")
 print("Só tô dando um jeito aqui, já já to ai...")
+
+#Print with a numbers
 print(10+8)
 print(20-2)
 print(6*3)
 print(36/2)
 print(2*3**2)
-Sales = 200
+
+#Variables
+Sales = 250
 cost = 50
+Meta = 150
 profit = Sales - cost
+
+#Print with a variable and a string
 print("Your profit is:", profit)
 print("Your cost is:", cost)
 print("Your sales are:", Sales)
+
+#Calculating with variables
 My_age = 5*3
 print("My age is:", My_age)
+
+#If, elif and else
+if profit == Meta:
+    print("Great Job")
+elif profit < Meta:
+    print("Not bad, but you can do better!")
+else:
+    print("excellent Work")
